a839e4dde58a57be42fd8729848d854b96163fbe
金展鹏 <48779675+JZP1337761329@users.noreply.github.com>
2019-05-22 22:35:15 +0800

    4th homework
    4th homework

---
+++ 4th homework.py
@@ -0,0 +1,14 @@
+Python 3.6.5 (v3.6.5:f59c0932b4, Mar 28 2018, 17:00:18) [MSC v.1900 64 bit (AMD64)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> 
+================= RESTART: C:/Users/shinelon/Desktop/154.py =================
+█Getting uuid of QR code.
+Downloading QR code.
+Please scan the QR code to log in.
+Please press confirm on your phone.
+Loading the contact, this may take a little while.
+Login successfully as 于无声处
+男性好友：69.05%
+女性好友：21.43%
+其他：11.90%
+>>> 
